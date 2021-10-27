7e8798e5c333bed50408622e3874951b0301e842
Angel <angelesquembre@gmail.com>
2021-10-27 17:58:13 +0200

    Final commit

--- src/dashboard_pages/home.py
+++ src/dashboard_pages/home.py
@@ -9,8 +9,12 @@ from matplotlib import pyplot as plt
 def app():
     cols = st.beta_columns(3)
     with cols[1]:
-    
+        st.title('Inicio')
         st.image('src/dashboard_pages/imgs/logo.png')
     
-    st.write('This is the *home* page.')
+    st.header('¿Qué es esto?')
+    st.write('HAR-Tastic es un modelo Long short-term memory.')
+
+    
+
     #Información genérica sobre el modelo, fotos, explicación de la DB
